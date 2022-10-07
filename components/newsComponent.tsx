9ee4b12a06f005ffe518961fcd3e9f13050a4dea
MateusRanzani <mateusranzani@hotmail.com>
2022-10-06 21:37:24 -0300

    Update news

--- components/newsComponent.tsx
+++ components/newsComponent.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { NextScript } from "next/document";
+import Link from "next/link";
 
 interface NewsPropsInterface {
   news: any;
@@ -17,12 +18,19 @@ function NewsComponent({ news }: NewsPropsInterface) {
   };
 
   return (
-    <div className="">
-      <div className="cardOfNew">
-        <p className="titleOfNew">{news.titulo}</p>
-        <p className="introducaoOfNew">{news.introducao}</p>
+    <>
+      <div className="">
+        <Link href={`${news.link}`}>
+          <a href="">
+            <div className="cardOfNew">
+              <p className="titleOfNew">{news.titulo}</p>
+              <p className="newIntroduction">{news.introducao}</p>
+            </div>
+          </a>{" "}
+        </Link>
       </div>
-    </div>
+      <div></div>
+    </>
   );
 }
 
