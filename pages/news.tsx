b16319893c55d5375290b433ca6c8855cb0f5370
Mateus Ranzani <mateusranzani@hotmail.com>
2022-10-31 22:14:04 -0300

    Update news page

--- pages/news.tsx
+++ pages/news.tsx
@@ -2,37 +2,34 @@ import { useSession } from "next-auth/react";
 
 import NewsComponent from "../components/newsComponent";
 
-export const getStaticProps = async () => {
-  const res = await fetch("http://servicodados.ibge.gov.br/api/v3/noticias/");
-  const data = await res.json();
-
-  return {
-    props: {
-      news: data,
-    },
-  };
-};
-
 const NewsPage = ({ news }: any) => {
   const { data: session, status } = useSession();
 
   return (
     <>
-      {session && (
-        <div className="totalPageNews">
-          <div className="contentNewsPage flex flex-wrap justify-center">
-            {news.items.map((thisNew: any, keyId: any) => {
-              return (
-                <div key={keyId}>
-                  <NewsComponent news={thisNew} />
-                </div>
-              );
-            })}
-          </div>
+      <div className="totalPageNews">
+        <div className="contentNewsPage flex flex-wrap justify-center">
+          {news.items.map((thisNew: any, keyId: any) => {
+            return (
+              <div key={keyId}>
+                <NewsComponent news={thisNew} />
+              </div>
+            );
+          })}
         </div>
-      )}
+      </div>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch("http://servicodados.ibge.gov.br/api/v3/noticias/");
+  const data = await res.json();
+  return {
+    props: {
+      news: data,
+    },
+  };
+}
 
 export default NewsPage;
