ea6691b3e03c46fd806effce153d6e8f0384789b
Mateus Ranzani <mateusranzani@hotmail.com>
2022-11-07 21:04:29 -0300

    Update navBar

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -9,50 +9,57 @@ const NavBar: NextPage = (props) => {
 
   return (
     <nav>
-      <div className="navBar flex justify-between items-center border-b-[1px] border-[#e6e5e5]">
-        <div className="flex items-center">
-          <div>
-            <Link href="/">
-              <div className="icon--logo">
-                <Image
-                  loader={myLoader}
-                  src="me.png"
-                  alt="Picture of the author"
-                  width={60}
-                  height={45}
-                />
-              </div>
-            </Link>
+      <div className="navBar flex justify-center items-center border-b-[1px] border-[#e6e5e5]">
+        <div
+          className="navBar_Content flex items-center justify-between"
+        >
+          <div className="flex items-center">
+            <div>
+              <Link href="/">
+                <div className="icon--logo">
+                  <Image
+                    loader={myLoader}
+                    src="me.png"
+                    alt="Picture of the author"
+                    width={60}
+                    height={45}
+                  />
+                </div>
+              </Link>
+            </div>
+
+            <div className="mr-10">
+              <Link href="/events">
+                <a className="--button">EVENTOS</a>
+              </Link>
+            </div>
+            <div className="mr-10">
+              <Link href="/news">
+                <a className="--button">NOTÍCIAS</a>
+              </Link>
+            </div>
           </div>
 
-          <div className="mr-10">
-            <Link href="/events">
-              <a className="--button">EVENTOS</a>
-            </Link>
-          </div>
-          <div className="mr-10">
-            <Link href="/news">
-              <a className="--button">NOTÍCIAS</a>
-            </Link>
+          <div className="flex items-center">
+            {!session && (
+              <button
+                className="border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200"
+                onClick={() => signIn("auth0")}
+              >
+                Login
+              </button>
+            )}
+            {session && (
+              <div>
+                <Link href="/profile">
+                  <a className="text-[#DF3A3A]" onClick={() => signOut()}>
+                    SAIR
+                  </a>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
-        {!session && (
-         <button
-         className="border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200"
-         onClick={() => signIn('auth0')}
-       >
-         Login
-       </button>
-        )}
-        {session && (
-          <div>
-            <Link href="/profile">
-              <a className="text-[#DF3A3A]" onClick={() => signOut()}>
-                SAIR
-              </a>
-            </Link>
-          </div>
-        )}
       </div>
     </nav>
   );
