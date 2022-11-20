947d051ff8b359e054aed732e252314f4455bc00
Mateus Ranzani <mateusranzani@hotmail.com>
2022-11-20 15:42:42 -0300

    Update

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,17 +1,18 @@
-import {NextApiRequest, NextApiResponse} from "next"
-import NextAuth from "next-auth"
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
 
-const options= {
+const options = {
   // Configure one or more authentication providers
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID!,
       clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.AUTH0_ISSUER
+      issuer: process.env.AUTH0_ISSUER,
     }),
   ],
   secret: process.env.SECRET,
-}
+};
 
-export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => NextAuth(req, res, options)
+export default (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
+  NextAuth(req, res, options);
