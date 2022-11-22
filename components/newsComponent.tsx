b9117103c1ee863f591dda6ff3e180a67def3db7
Mateus Ranzani <mateusranzani@hotmail.com>
2022-11-21 21:50:34 -0300

    Update

--- components/newsComponent.tsx
+++ components/newsComponent.tsx
@@ -1,10 +1,21 @@
 function NewsComponent({ news }: any) {
   return (
     <>
-    {/* #04D361 */}
+      {/* #04D361 */}
       <div className="">
         <a href={`${news.link}`} target="_blank">
-          <div className="cardOfNew" style={{backgroundColor:`${news.tipo === "Notícia"? "rgba(4, 211, 97, 0.45)" : news.tipo === "Release"? "rgba(255, 153, 0, 0.45)" : "rgba(255, 153, 0, 0.1)" }`}}>
+          <div
+            className="cardOfNew"
+            style={{
+              backgroundColor: `${
+                news.tipo === "Notícia"
+                  ? "rgba(4, 211, 97, 0.45)"
+                  : news.tipo === "Release"
+                  ? "rgba(255, 153, 0, 0.45)"
+                  : "rgba(255, 153, 0, 0.1)"
+              }`,
+            }}
+          >
             <p className="titleOfNew">{news.titulo}</p>
             <p className="newIntroduction">{news.introducao}</p>
           </div>
--- interfaces/UserInterface.tsx
+++ interfaces/UserInterface.tsx
@@ -2,14 +2,14 @@ import ComplaintsInterface from "./ComplaintsInterface";
 import EventsInterface from "./EventsInterface";
 
 interface UserInterface {
-    _id: string;
-    name: string;
-    email: string;
-    cellphone: string;
-    city: string;
-    country: string;
-    events: EventsInterface[];
-    complaints: ComplaintsInterface[];
+  _id: string;
+  name: string;
+  email: string;
+  cellphone: string;
+  city: string;
+  country: string;
+  events: EventsInterface[];
+  complaints: ComplaintsInterface[];
 }
 
 export default UserInterface;
