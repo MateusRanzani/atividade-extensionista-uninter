06ea023486e7876e772e2153f8cc61ac58ad6a01
MateusRanzani <mateusranzani@hotmail.com>
2023-01-28 16:28:07 -0300

    update project

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import ReactTooltip from "react-tooltip";
 import Modal from "react-modal";
@@ -93,206 +93,202 @@ const Events: NextPage = () => {
     }
   };
 
-  const isEmptyObj = (obj: any) => {
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-        return false;
-      }
-    }
-
-    return true;
-  };
-
   function closeModal() {
     setIsOpen(false);
   }
-
+  
   useEffect(() => {
     fetchUser();
     getAllEvents();
   }, []);
 
   return (
-    <div className="totalPageNews">
-      <div className="contentNewsPageEvent">
-        <div className="text-center relative w-full h-[50px] bg-[#04D361] text-[white] text-[35px] rounded-[30px]">
-          <b>EVENTOS</b>
+  <>
+    {session && (
+ 
+      <div className="totalPageNews">
+        <div className="contentNewsPageEvent">
+          <div className="text-center relative w-full h-[50px] bg-[#04D361] text-[white] text-[35px] rounded-[30px]">
+            <b>EVENTOS</b>
 
-          <button
-            data-tip="Criar um evento"
-            data-for="test"
-            data-place="left"
-            className="absolute  right-[20px] m-auto top-[0px] bottom-[0px] text-[10px] whitespace-nowrap"
-            onClick={() => setIsOpen((state) => true)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6 "
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6v12m6-6H6"
-              />
-            </svg>
-          </button>
-          <ReactTooltip id="test">{}</ReactTooltip>
-        </div>
-
-        <div className="mt-10 pb-5">
-          {allEvents.map((events) => (
-            <div className="w-full p-3 mt-10 h-[250px] bg-[url('https://images5.alphacoders.com/568/thumb-1920-568879.jpg')]  text-[35px] rounded-[30px] relative">
-              <div className="max-w-[50%] font-bold p-2 truncate bg-[#04D361] text-[white] text-[23px] rounded-[30px] absolute top-[-20px]">
-                {events.event_name}
-              </div>
-
-              <div className="   h-[15%] bg-[white]/[0.5]  p-1 absolute  right-[20px] rounded-[30px] text-[18px]">
-                {!enterTheEvent ? (
-                  <button
-                    className="flex whitespace-nowrap border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200  absolute  right-[20px]"
-                    onClick={() => setEnterTheEvent((state) => true)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6  mr-1 "
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                      />
-                    </svg>
-                    Participar do Evento
-                  </button>
-                ) : (
-                  <button
-                    className="flex whitespace-nowrap border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200  absolute  right-[20px]"
-                    onClick={() => setEnterTheEvent((state) => false)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6 mr-1"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                      />
-                    </svg>
-                    Sair do Evento
-                  </button>
-                )}
-              </div>
-
-              <div className="w-[50%]  h-[70%] bg-[white]/[0.5]  p-1 absolute bottom-[20px] right-[20px] rounded-[30px] text-[18px] overflow-auto">
-                <div className="text-center font-semibold">DESCRIÇÃO</div>
-                <div className="bg-[white]/[0.5]  p-2 rounded-[25px] text-[16px] max-h-[50%] overflow-auto">
-                  {events.description}
-                </div>
-
-                <div className="text-center font-semibold">Data</div>
-                <div className="text-center text-[16px]">
-                  {events.date}
-                  <br />
-                  {countOfMembers(events)}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
-        className="Modal "
-        overlayClassName="Overlay"
-      >
-        <div className="w-[100%] h-[100%] p-10">
-          <div className="w-[100%] text-center  ">
-            <p> Nome do evento: </p>
-            <input
-              type="text"
-              className="w-[30%] border-2 border-[#04D361] rounded-[5px] "
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setEventName((state) => e.target.value)
-              }
-              value={eventName}
-            />
-          </div>
-
-          <div className="w-[100%] text-center   mt-2">
-            <p> Descrição do evento: </p>
-            <input
-              type="text"
-              className="w-[30%] border-2 border-[#04D361] rounded-[5px] "
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setEventDescription((state) => e.target.value)
-              }
-              value={eventDescription}
-            />
-          </div>
-
-          <div className="w-[100%] text-center   mt-2">
-            <p> Data: </p>
-            <input
-              type="text"
-              className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setDateEvent((state) => e.target.value)
-              }
-              value={dateEvent}
-            />
-          </div>
-
-          <div className="w-[100%] text-center   mt-2">
-            <p> Cidade: </p>
-            <input
-              type="text"
-              className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setCityEvent((state) => e.target.value)
-              }
-              value={cityEvent}
-            />
-          </div>
-
-          <div className="w-[100%] text-center   mt-2">
-            <p> País: </p>
-            <input
-              type="text"
-              className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setCountryEvent((state) => e.target.value)
-              }
-              value={countryEvent}
-            />
-          </div>
-
-          <div className="w-[100%] text-center   mt-2">
             <button
-              className="border-l-2 border-2 px-4 rounded-lg bg-[#04D361] hover:opacity-80 transition duration-200"
-              onClick={() => createNewEvent()}
+              data-tip="Criar um evento"
+              data-for="test"
+              data-place="left"
+              className="absolute  right-[20px] m-auto top-[0px] bottom-[0px] text-[10px] whitespace-nowrap"
+              onClick={() => setIsOpen((state) => true)}
             >
-              CRIAR EVENTO
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v12m6-6H6"
+                />
+              </svg>
             </button>
+            <ReactTooltip id="test">{}</ReactTooltip>
+          </div>
+
+          <div className="mt-10 pb-5">
+            {allEvents.map((events) => (
+              <div className="w-full p-3 mt-10 h-[250px] bg-[url('https://images5.alphacoders.com/568/thumb-1920-568879.jpg')]  text-[35px] rounded-[30px] relative">
+                <div className="max-w-[50%] font-bold p-2 truncate bg-[#04D361] text-[white] text-[23px] rounded-[30px] absolute top-[-20px]">
+                  {events.event_name}
+                </div>
+
+                <div className="   h-[15%] bg-[white]/[0.5]  p-1 absolute  right-[20px] rounded-[30px] text-[18px]">
+                  {!enterTheEvent ? (
+                    <button
+                      className="flex whitespace-nowrap border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200  absolute  right-[20px]"
+                      onClick={() => setEnterTheEvent((state) => true)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6  mr-1 "
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                        />
+                      </svg>
+                      Participar do Evento
+                    </button>
+                  ) : (
+                    <button
+                      className="flex whitespace-nowrap border-l-2 border-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200  absolute  right-[20px]"
+                      onClick={() => setEnterTheEvent((state) => false)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6 mr-1"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                        />
+                      </svg>
+                      Sair do Evento
+                    </button>
+                  )}
+                </div>
+
+                <div className="w-[50%]  h-[70%] bg-[white]/[0.5]  p-1 absolute bottom-[20px] right-[20px] rounded-[30px] text-[18px] overflow-auto">
+                  <div className="text-center font-semibold">DESCRIÇÃO</div>
+                  <div className="bg-[white]/[0.5]  p-2 rounded-[25px] text-[16px] max-h-[50%] overflow-auto">
+                    {events.description}
+                  </div>
+
+                  <div className="text-center font-semibold">Data</div>
+                  <div className="text-center text-[16px]">
+                    {events.date}
+                    <br />
+                    {countOfMembers(events)}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      </Modal>
-    </div>
+
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          className="Modal "
+          overlayClassName="Overlay"
+        >
+          <div className="w-[100%] h-[100%] p-10">
+            <div className="w-[100%] text-center  ">
+              <p> Nome do evento: </p>
+              <input
+                type="text"
+                className="w-[30%] border-2 border-[#04D361] rounded-[5px] "
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEventName((state) => e.target.value)
+                }
+                value={eventName}
+              />
+            </div>
+
+            <div className="w-[100%] text-center   mt-2">
+              <p> Descrição do evento: </p>
+              <input
+                type="text"
+                className="w-[30%] border-2 border-[#04D361] rounded-[5px] "
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEventDescription((state) => e.target.value)
+                }
+                value={eventDescription}
+              />
+            </div>
+
+            <div className="w-[100%] text-center   mt-2">
+              <p> Data: </p>
+              <input
+                type="text"
+                className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDateEvent((state) => e.target.value)
+                }
+                value={dateEvent}
+              />
+            </div>
+
+            <div className="w-[100%] text-center   mt-2">
+              <p> Cidade: </p>
+              <input
+                type="text"
+                className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCityEvent((state) => e.target.value)
+                }
+                value={cityEvent}
+              />
+            </div>
+
+            <div className="w-[100%] text-center   mt-2">
+              <p> País: </p>
+              <input
+                type="text"
+                className="w-[15%] border-2 border-[#04D361] rounded-[5px] "
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCountryEvent((state) => e.target.value)
+                }
+                value={countryEvent}
+              />
+            </div>
+
+            <div className="w-[100%] text-center   mt-2">
+              <button
+                className="border-l-2 border-2 px-4 rounded-lg bg-[#04D361] hover:opacity-80 transition duration-200"
+                onClick={() => createNewEvent()}
+              >
+                CRIAR EVENTO
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+      )}
+    </>
+    
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -62,27 +62,19 @@ const Home: NextPage = () => {
             <br />
             <button
               onClick={() => signIn("auth0")}
-              className="rounded-full text-[1.8rem]  bg-[#FF9900] hover:[#df8703 ] text-[white] mx-3 px-5 py-1 hover:bg-[#da8402] duration-300"
+              className="buttonsIndexPage rounded-full text-[1.8rem]  bg-[#FF9900] hover:[#df8703 ] text-[white] mx-3 px-5 py-1 hover:bg-[#da8402] duration-300"
             >
               CADASTRAR
             </button>
             <button
               onClick={(): Promise<void> => signIn()}
-              className="rounded-full text-[1.8rem]  bg-[#04D361] text-[white] mx-3 px-5 py-1 hover:bg-[#01b653] duration-300 "
+              className="buttonsIndexPage rounded-full text-[1.8rem]  bg-[#04D361] text-[white] mx-3 px-5 py-1 hover:bg-[#01b653] duration-300 "
             >
               FAZER LOGIN
             </button>
           </div>
         </div>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            flex: "1",
-            position: "relative",
-            justifyContent: "center",
-          }}
-        >
+        <div className="footerHeader">
           <div className="absolute bottom-0 text-center">
             <p className="text-[1rem]">
               O objetivo deste projeto é de utilizar a tecnologia como uma
@@ -112,93 +104,64 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="projectInformation--index1">
-        <div className="informationLeftSide--index">
-          <div style={{ height: "fit-content", display: "inline-flex" }}>
-            <img
-              src="/assets/homePage/image1.svg"
-              alt=""
-              style={{ width: "25rem" }}
-            />
-            <img
-              className="svgBlobMiniature flex absolute"
-              src="/assets/homePage/image2.svg"
-              alt=""
-              style={{
-                width: "25%",
-                height: "52%",
-                position: "absolute",
-                bottom: "0rem",
-                right: "8rem",
-                display: "inline-block",
-              }}
-            />
+      <div className="secondContainer">
+        <div className="headerSecondContainer">
+          <div className="gridTemplateBanner1">
+            <div className="contentImagesBanner1">
+              <img
+                className="informationLeftSideContentOneImg"
+                src="/assets/homePage/image1.svg"
+                alt=""
+              />
+              <img
+                className="svgBlobMiniature flex absolute"
+                src="/assets/homePage/image2.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="contentTextBanner1">
+            As mudanças climáticas são um fenômeno global que tem impactos
+            significativos em economias e comunidades em todo o mundo. Segundo o
+            Painel Intergovernamental de Mudanças Climáticas (IPCC), a
+            temperatura global pode aumentar entre 1,8ºC e 4,0ºC nos próximos
+            100 anos, e o nível médio do mar pode subir entre 0,18m e 0,59m, o
+            que pode afetar seriamente as atividades humanas e os ecossistemas
+            terrestres. Essas mudanças são intensificadas pela ação humana.
           </div>
         </div>
+        <div className="footerSecondContainer">
+          <div className="contentTextBanner1">
+            O objetivo deste projeto é empregar a tecnologia para aumentar a
+            conscientização sobre as mudanças climáticas e possibilitar meios
+            mais fáceis para a denúncia de focos de incêndio e desmatamento
+            ilegal. Este projeto foi planejado com base na 13ª ODS da ONU, que
+            busca promover ações contra a mudança global do clima.
+          </div>
+          <div>
+            <div className="contentImagesBanner1">
+              <img
+                className="informationRightSideContentTwoImg"
+                src="/assets/homePage/image3.svg"
+                alt=""
+              />
+              <img
+                className="informationRightSideContentTwoImg_"
+                src="/assets/homePage/image4.svg"
+                alt=""
+              />
 
-        <div className="informationRigthSide--index text-center">
-          As mudanças climáticas são um evento transnacional, cujo impactos
-          estão desregulando economias nacionais e afetando pessoas em todos os
-          lugares do mundo. Projeções do Painel Intergovernamental de Mudanças
-          Climáticas (IPCC) indicam que nos próximos 100 anos poderá haver um
-          aumento da temperatura média global entre 1,8ºC e 4,0ºC. e um aumento
-          no nível médio do mar entre 0,18m e 0,59m, o que pode afetar
-          significativamente as atividades humanas e os ecossistemas terrestres.
-          Atualmente, essas mudanças têm ocorrido de forma intensa em razão da
-          ação do homem.
-        </div>
-      </div>
-
-      <div className="projectInformation--index2">
-        <div className="informationLeftSide-two--index flex items-center text-center ">
-          O objetivo deste projeto é de utilizar a tecnologia como uma aliada na
-          conscientização sobre as mudanças climáticas, assim como possibilitar
-          e facilitar os meios de denúncias, tanto para focos de incêndio quanto
-          para desmatamento ilegal.
-          <br />
-          <br />O planejamento foi feito com base na 13ª ODS da ONU – Ação
-          contra a mudança global do clima
-        </div>
-
-        <div className="informationRigthSide-two--index ">
-          <div style={{ height: "fit-content", display: "inline-flex" }}>
-            <img
-              src="/assets/homePage/image3.svg"
-              alt=""
-              style={{
-                width: "30rem",
-              }}
-            />
-
-            <img
-              src="/assets/homePage/image4.svg"
-              alt=""
-              style={{
-                width: "30%",
-                height: "52%",
-                position: "absolute",
-                bottom: "-0.1rem",
-                left: "-0.4rem",
-                display: "inline-block",
-              }}
-            />
-
-            <img
-              src="/assets/homePage/image5.svg"
-              className="svgBlobMiniature--two flex absolute"
-              alt=""
-              style={{
-                width: "30%",
-                height: "52%",
-                position: "absolute",
-                top: "-0.1rem",
-                right: "1rem",
-                display: "inline-block",
-              }}
-            />
+              <img
+                src="/assets/homePage/image5.svg"
+                className="svgBlobMiniature--two flex absolute"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
+
+    
     </div>
   );
 };
